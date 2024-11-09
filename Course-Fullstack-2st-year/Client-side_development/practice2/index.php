9ce3ceb43e4b74fef9a631031091a15fe52a34a4
NikitaR041 <nikitar041@gmail.com>
2024-11-09 22:16:16 +0500

    add files php and css

---
+++ Course-Fullstack-2st-year/Client-side_development/practice2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html> 
+<html>
+<head>
+    <title>Главный экран</title>
+    <link href="/style.css" rel="stylesheet" />
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header">
+                <img class="logo" src="" alr="">
+                <h1>Добро пожаловать на страницу "Мир танков"</h1>
+                <p>Это пример текста.</p>
+            </div>
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+    
+    </footer>
+</body>
+</html>
