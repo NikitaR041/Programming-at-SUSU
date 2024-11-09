3622a533e8d302da0d07b20111a7cca97324b30b
NikitaR041 <nikitar041@gmail.com>
2024-11-10 00:20:42 +0500

    update practice5 C#

---
+++ 3-semester/ProjectC#/practice5/AudioSpeaker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS
+{
+    internal class AudioSpeaker
+    {
+        private string name_audio_speaker; //Название аудиоколонки
+        private string power_speaker; //Мощность аудиоколонки
+        private string size_speaker; //Размеры аудиоколонки
+        private string connection_type; //Тип соединения аудиоколонки
+        private string battery_life; //Время работы от батареи аудиоколонки
+        private byte price; //Цена аудиоколонки
+
+        public AudioSpeaker(string name_audio_speaker, string power_speaker, string counsize_speakertry, string connection_type, string battery_life, byte price)
+        {
+            this.name_audio_speaker = name_audio_speaker;
+            this.power_speaker = power_speaker;
+            this.size_speaker = size_speaker;
+            this.connection_type = connection_type;
+            this.battery_life = battery_life;
+            this.price = price;
+        }
+
+
+
+    }
+}
--- 3-semester/ProjectC#/practice5/Program.cs
+++ 3-semester/ProjectC#/practice5/Program.cs
@@ -2,41 +2,24 @@
 using System.IO;
 using System.Security.AccessControl;
 
-class Program {
-    //Пробуем метод, который считывает важную информацию из <td> и <\td> в двумерный список
-    static void SearchText(List<List<string>> obj1, StreamReader obj2)
-    {
-        //Пробуем построчно считать
-        string line;
-        int index = 0;
-        while (true) {
-            line = obj2.ReadLine();
-            if (!string.IsNullOrEmpty(line) && line[0] == '<' && line[line.Length - 1] == '>')
+namespace AIS 
+{
+    class Programm{
+        static void Main()
+        {
+            string path = "Example.txt";  // путь к файлу
+            string content = File.ReadAllText(path); // Считать всё содержимое в одну строку
+            Console.WriteLine(content);
+
+            // или считать построчно
+            string[] lines = File.ReadAllLines(path);
+            foreach (var line in lines)
             {
-                continue;
+                Console.WriteLine(line);
             }
-            else {
-                obj1[index].Add(line);
-                index++;
-            }
+            //fsr.Close();    
+            //fs.Close();
+            Console.ReadKey();
         }
-
-    }
-    static void Main() {
-        FileStream fs = new FileStream("Example.txt", FileMode.Open);
-        StreamReader fsr = new StreamReader(fs);
-
-        List<List<string>> importantElements = new List<List<string>>();
-
-        SearchText(importantElements, fsr);
-
-        foreach (var elem in importantElements) { 
-        
-            }
-        
-        
-        fsr.Close();
-        fs.Close();
-        Console.ReadKey();
     }
 }
