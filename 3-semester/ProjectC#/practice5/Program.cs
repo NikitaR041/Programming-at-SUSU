82ef3f5be7cab8fa13a574062452c6b48f516e6c
NikitaR041 <nikitar041@gmail.com>
2024-11-10 17:58:45 +0500

    update C#

--- 3-semester/ProjectC#/practice5/Program.cs
+++ 3-semester/ProjectC#/practice5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.AccessControl;
 
 namespace AIS 
@@ -7,18 +8,48 @@ namespace AIS
     class Programm{
         static void Main()
         {
-            string path = "Example.txt";  // путь к файлу
-            string content = File.ReadAllText(path); // Считать всё содержимое в одну строку
-            Console.WriteLine(content);
+            FileStream fs = new FileStream("Example.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
 
-            // или считать построчно
-            string[] lines = File.ReadAllLines(path);
-            foreach (var line in lines)
+            List<string> mass = new List<string>();
+            string stroka = sr.ReadLine();
+
+            //Console.WriteLine(stroka);
+            byte index = 0;
+            //Console.WriteLine(stroka);
+            //Считываем информацию из файла, пока не дошли доконца
+            while (!sr.EndOfStream)
             {
-                Console.WriteLine(line);
+                //Пробный вариант
+                if (stroka.IndexOf("<tr>") != -1 || stroka.IndexOf("</tr>") != -1)
+                {
+                    stroka = sr.ReadLine();
+                    continue;
+                }
+                else if (stroka.IndexOf("<td>") != -1)
+                {
+                    stroka = stroka.Substring(stroka.IndexOf("<td>") + 4, stroka.LastIndexOf("</td>") - stroka.IndexOf("<td>")-4).Trim();
+                    //mass = mass.Insert(index, stroka);
+                    stroka = stroka.Trim();
+                    mass.Add(stroka);
+                    stroka = sr.ReadLine();
+                    index++;
+                }
+                else {
+                    continue;
+                }
             }
-            //fsr.Close();    
-            //fs.Close();
+
+            //int h = stroka.IndexOf("<tr>");
+            //Console.WriteLine(h);
+
+            //Console.WriteLine(stroka);
+            foreach (var elem in mass) {
+                Console.WriteLine(elem);
+            }
+
+            fs.Close();
+            sr.Close();
             Console.ReadKey();
         }
     }
