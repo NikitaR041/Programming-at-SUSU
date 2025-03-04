61988c5a1d8acaf84deb184eed45fb47a2dae3f4
NikitaR041 <nikitar041@gmail.com>
2025-03-05 00:10:35 +0500

    udpate c++

--- AlgorithmAndStructures/LabWorks/LabWorks.cpp
+++ AlgorithmAndStructures/LabWorks/LabWorks.cpp
@@ -7,7 +7,38 @@
 /*
 Задание 1
 Реализуйте АТД Стек на односвязном списке (forward_list).
+
+Решение:
+1)Нужно реализовать узел
+2)Реализовать методы 
+
 */
+
+class Stack {
+private:
+    struct Node {
+        int value; //Значение узла
+        std::unique_ptr<Node> next; //Указатель на следующий элемент
+        Node(int value) : value(value), next(nullptr) {}
+    };
+
+    std::unique_ptr<Node> top; //Указатель на верхний элемент
+public:
+    Stack() : top(nullptr) {}
+    
+    //Реализовать методы:
+    void pushElem(int value) {
+        std::unique_ptr<Node> obj = std::make_unique<Node>(value);
+        obj->next = std::move(top);
+        top = std::move(obj);
+    }
+
+    //Исправить
+    void popElen() {
+        if (top != nullptr) top;
+    }
+};
+
 
 int main()
 {
