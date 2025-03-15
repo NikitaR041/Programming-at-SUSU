ac2ad8dcca6f1ecb34d7bd256a1b538af7ecbdf1
NikitaR041 <nikitar041@gmail.com>
2025-03-15 14:04:56 +0500

    udpate Java les4

--- LearningJava/les4/src/Main.java
+++ LearningJava/les4/src/Main.java
@@ -15,16 +15,32 @@ https://metanit.com/java/tutorial/9.1.php
 
 Задание - Лямбда-выражение.
 4.	Написать функциональный интерфейс с методом, который принимает число и возвращает булево значение. Написать реализацию такого интерфейса в виде лямбда-выражения, которое возвращает true если переданное число делится без остатка на 13.
-5.	 Написать функциональный интерфейс с методом, который принимает две строки и возвращает тоже строку. Написать реализацию такого интерфейса в виде лямбды, которая возвращает ту строку, которая длиннее.
+5.  Написать функциональный интерфейс с методом, который принимает две строки и возвращает тоже строку. Написать реализацию такого интерфейса в виде лямбды, которая возвращает ту строку, которая длиннее.
 6.	Написать функциональный интерфейс с методом, который принимает три дробных числа: a, b, c и возвращает тоже дробное число. Написать реализацию такого интерфейса в виде лямбда-выражения, которое возвращает дискриминант. Кто забыл, D = b^2 — 4ac.
 7.	Используя функциональный интерфейс из задачи 6 написать лямбда-выражение, которое возвращает результат операции a * b^c.
 
- */
-
-//Задание 2 | Регулярные выражение - выбран рандомайзером
+*/
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.*;
 
+//Задание - Регулярные выражения.
+//#1
+/*
+public class Main {
+    public static void main(String[] args) {
+        String example = "VseMoguchi@yandex.ru Tamagochi2004@mail.ru niki228@gmail.com test@test.test Oshibka1test.test Oshibka2@.";
+        Pattern pattern = Pattern.compile("\\b[\\w]+@[\\w]+\\.[a-zA-Z]{2,6}\\b"); // Для простых видов e-mail
+        //Pattern pattern = Pattern.compile("\\b[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}\\b"); //Для более сложных видов e-mail
+        Matcher matcher = pattern.matcher(example);
+        while (matcher.find()) {
+            System.out.println("Найден email: " + matcher.group());
+        }
+    }
+}
+*/
+//#2
+/*
 public class Main {
     public static void main(String[] args) {
         String example = "функция функция   функция    функция функция";
@@ -34,3 +50,26 @@ public class Main {
         System.out.println(new_example);
     }
 }
+*/
+//#3
+/*
+public class Main{
+    public static void findFrequencyWords(String obj){
+        Map<String, Integer> states = new HashMap<String, Integer>();
+        Pattern pattern = Pattern.compile("\\s+");
+        String[] words = pattern.split(obj);
+        for (String word : words) {
+            states.put(word, states.getOrDefault(word, 0) + 1);
+        }
+        for(Map.Entry<String, Integer> item : states.entrySet()){
+            System.out.printf("Ключ: %s  Значение: %d \n", item.getKey(), item.getValue());
+        }
+    }
+    public static void main(String[] args){
+        String example1 = "Шла Саша по шоссе и сосала сушку";
+        String example2 = "Абракадабра Абракадабра Абракадабра Саша саша Мама Мама";
+        findFrequencyWords(example1);
+        System.out.println("----------------------");
+        findFrequencyWords(example2);
+    }
+}*/
