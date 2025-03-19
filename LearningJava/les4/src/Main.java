2c403ba82166505a3871f66a1f0f1695d3504216
NikitaR041 <nikitar041@gmail.com>
2025-03-20 01:21:57 +0500

    udpate les4

--- LearningJava/les4/src/Main.java
+++ LearningJava/les4/src/Main.java
@@ -14,9 +14,12 @@ https://metanit.com/java/tutorial/9.1.php
 3.	Написать функцию, которая при помощи регулярных выражений разбивает текст на отдельные слова и находит частоту встречаемости отдельных слов.
 
 Задание - Лямбда-выражение.
-4.	Написать функциональный интерфейс с методом, который принимает число и возвращает булево значение. Написать реализацию такого интерфейса в виде лямбда-выражения, которое возвращает true если переданное число делится без остатка на 13.
-5.  Написать функциональный интерфейс с методом, который принимает две строки и возвращает тоже строку. Написать реализацию такого интерфейса в виде лямбды, которая возвращает ту строку, которая длиннее.
-6.	Написать функциональный интерфейс с методом, который принимает три дробных числа: a, b, c и возвращает тоже дробное число. Написать реализацию такого интерфейса в виде лямбда-выражения, которое возвращает дискриминант. Кто забыл, D = b^2 — 4ac.
+4.	Написать функциональный интерфейс с методом, который принимает число и возвращает булево значение.
+Написать реализацию такого интерфейса в виде лямбда-выражения, которое возвращает true если переданное число делится без остатка на 13.
+5.  Написать функциональный интерфейс с методом, который принимает две строки и возвращает тоже строку.
+Написать реализацию такого интерфейса в виде лямбды, которая возвращает ту строку, которая длиннее.
+6.	Написать функциональный интерфейс с методом, который принимает три дробных числа: a, b, c и возвращает тоже дробное число.
+Написать реализацию такого интерфейса в виде лямбда-выражения, которое возвращает дискриминант. Кто забыл, D = b^2 — 4ac.
 7.	Используя функциональный интерфейс из задачи 6 написать лямбда-выражение, которое возвращает результат операции a * b^c.
 
 */
@@ -73,3 +76,65 @@ public class Main{
         findFrequencyWords(example2);
     }
 }*/
+
+//Задание - Лямбда-выражение.
+//#4
+/*
+interface Operation{
+    boolean isDivion(int number);
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Operation operation = number -> number % 13 == 0;
+        System.out.println(operation.isDivion(15));
+        System.out.println(operation.isDivion(26));
+        System.out.println(operation.isDivion(39));
+        System.out.println(operation.isDivion(13));
+    }
+}
+*/
+
+//#5
+/*
+interface Operation{
+    String obj(String a, String b);
+}
+
+public class Main{
+    public static void main(String[] args){
+        Operation obj = (a,b) -> a.length() >= b.length() ? a : b;
+        System.out.println(obj.obj("Hello", "World!")); // World!
+        System.out.println(obj.obj("Java", "Programming")); // Programming
+        System.out.println(obj.obj("Car", "Fly")); // Functions
+    }
+}
+*/
+
+//#6
+/*
+interface Discriminant{
+    double D(double a, double b, double c);
+}
+public class Main{
+    public static void main(String[] args){
+        Discriminant obj = (a,b,c) -> (b*b) - 4*a*c;
+        System.out.println(obj.D(1,-8,12));
+        System.out.println(obj.D(5,3,7));
+        System.out.println(obj.D(1,-6,9));
+    }
+}
+*/
+
+//#7
+interface Discriminant{
+    double D(double a, double b, double c);
+}
+public class Main{
+    public static void main(String[] args){
+        Discriminant obj = (a,b,c) -> a * Math.pow(b,c);
+        System.out.println(obj.D(1,-8,2));
+        System.out.println(obj.D(5,3,3));
+        System.out.println(obj.D(2,4,0.5));
+    }
+}
