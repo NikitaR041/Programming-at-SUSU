b9920af63c7b89dd4a8f58526931240931788e73
NikitaR041 <nikitar041@gmail.com>
2025-04-22 14:13:43 +0500

    Add files bd

--- DataBase/TesstMyBD/Form1.Designer.cs
+++ DataBase/TesstMyBD/Form1.Designer.cs
@@ -28,20 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ДА!!!!";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(83, 145);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Я ГЕЙ?!";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(347, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(978, 571);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- DataBase/TesstMyBD/Form1.cs
+++ DataBase/TesstMyBD/Form1.cs
@@ -8,13 +8,69 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Подключение к копиям бд
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Security.Cryptography;
+
 namespace TesstMyBD
 {
     public partial class Form1 : Form
     {
+        private static string DS = "Database = Stud_WareHouse; User ID = Stud; Password = New20@%PSW;";
+
         public Form1()
         {
+
             InitializeComponent();
+
+        }
+            private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                DS = DS.Insert(0, "Data Source = susu.prima.susu.ac.ru, 51435; ");
+            }
+            else
+            {
+                DS = DS.Insert(0, "Data Source = MSSQL-2K8\\MSSQL_K; ");
+            }
+            using (SqlConnection connection = new SqlConnection(DS))
+            {
+                connection.Open();
+                MessageBox.Show($"Свойства подключения:\n" +
+                $"\tСтрока подключения: {connection.ConnectionString}\n" +
+                $"\tБаза данных: {connection.Database}\n" +
+                $"\tСервер: {connection.DataSource}\n" +
+                $"\tВерсия сервера: {connection.ServerVersion}\n" +
+                $"\tСостояние: {connection.State}\n" +
+                $"\tWorkstationld: {connection.WorkstationId}");
+            }
+
+            using (SqlConnection Connection = new SqlConnection(@DS))
+            {
+                Connection.Open();
+                string MeasUnitName = "руб";
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT \r\n    T.Tovar_ID,\r\n    T.TovarName,\r\n    T.IsTovar,\r\n    T.Amount,\r\n    T.Price,\r\n    T.Photo,\r\n    T.MeasUnit_ID,\r\n    M.MeasUnitName,\r\n    T.Parent_ID,\r\n    (T.Amount * T.Price) AS SumCost\r\nFROM \r\n    Tovar T\r\nJOIN \r\n    MeasUnit M ON T.MeasUnit_ID = M.MeasUnit_ID\r\nWHERE \r\n    M.MeasUnitName = @MeasUnitName AND T.IsTovar = 1;\r\n", Connection);
+               
+                DataSet ds1 = new DataSet();
+                adapter.Fill(ds1);
+                dataGridView1.DataSource = ds1.Tables[0];
+                dataGridView1.AutoResizeColumns();
+                dataGridView1.AutoSizeColumnsMode =
+                DataGridViewAutoSizeColumnsMode.AllCells;
+            }
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
